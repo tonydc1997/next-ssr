5f0161e5b660d99dba8f20979de39ea59a85edc0
Anthony Curtis <tonydc1997@gmail.com>
2018-09-27 11:49:05 -0700

    Import image

--- components/image.js
+++ components/image.js
@@ -1,5 +1,5 @@
 const Image = () => (
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMoTo33FQVstiCGT6kxjg2hkT8qVWK0DBrHUUUa9RIEdxcCbcjKA" alt="A cherry-red Tesla Model 3"/>
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAEDvATPcT2fYmxKprTK1rjFxnTS6YQoxD9n2Ap_fUgYMtCrRS" alt="A cherry-red Tesla Model 3"/>
 );
 
 export default Image;
--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from '../components/image';
 
 const About = () => {
   return (
