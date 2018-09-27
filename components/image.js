5d08a0acf0b555f4a8ade807b6ddeb78111b4678
Anthony Curtis <tonydc1997@gmail.com>
2018-09-27 11:45:12 -0700

    Create and export Image

---
+++ components/image.js
@@ -0,0 +1,5 @@
+const Image = () => (
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMoTo33FQVstiCGT6kxjg2hkT8qVWK0DBrHUUUa9RIEdxcCbcjKA" alt="A cherry-red Tesla Model 3"/>
+);
+
+export default Image;
