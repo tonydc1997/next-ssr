7fbd65549b28000498d204def660c3f5eb373ac2
Anthony Curtis <tonydc1997@gmail.com>
2018-09-27 11:18:51 -0700

    Incorporate next.js's link tag

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import Link from 'next/link';
 const Index = () => (
   <div>
     <h1>SSR MAGICIAN</h1>
-    <a href="/about">About</a>
+    <Link href='/about'>
+      <button>About</button>
+    </Link>
   </div>
 );
 
